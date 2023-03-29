1675ad4d724babd08cb82bc5167dbc678527564f
KevinBett <kevinbett458@gmail.com>
2023-03-29 19:46:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function reducer(batteryList) {
   return batteryList.reduce((acc, curr) => acc + curr, 0);
 }
 
-const batteries = [10, 5, 6, 4, 3, 2, 1];
+const batteries = [4, 5, 3, 4, 4, 6, 5];
 const totalBatteries = reducer(batteries);
 console.log(totalBatteries);
 
